9c6f7f52d82bee962987863ac9b95f214c152264
Alexander Kryuk <kryukalexander@gmail.com>
2018-02-09 13:05:36 +0600

    Migrated to pug templates instead of posthtml

--- posthtml.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    plugins: [
-        require('posthtml-include')({ root: './src/templates/includes'}),
-        require('posthtml-expressions')({locals: { test: 'Hello World!'} }),
-    ]
-};
